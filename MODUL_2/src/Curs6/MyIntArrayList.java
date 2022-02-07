6c06f45870739ecebedb1d4cf2d1bd5fb359fa4c
Ghioca Alexandru <ghioca.alex@yahoo.com>
2022-02-07 22:33:04 +0200

    07.02.2022

--- MODUL_2/src/Curs6/MyIntArrayList.java
+++ MODUL_2/src/Curs6/MyIntArrayList.java
@@ -8,20 +8,18 @@ public class MyIntArrayList {
   private int size;
 
 
-
   public MyIntArrayList() {
     this(10);
   }
 
 
-  public MyIntArrayList(MyIntArrayList c){
+  public MyIntArrayList(MyIntArrayList c) {
     this.size = c.size;
     this.elements = new int[this.size + 1];
-    for (int i = 0; i < this.size; i++){
+    for (int i = 0; i < this.size; i++) {
       this.elements[i] = c.get(i);
     }
   }
-
 
 
   public MyIntArrayList(int initialCapacity) {
@@ -38,7 +36,7 @@ public class MyIntArrayList {
       }
       elements[index] = e;
       this.size++;
-    }else{
+    } else {
       System.out.println("Throwing IndexOutOfBoundsException when trying to" +
               " add element at position " + index);
     }
@@ -54,17 +52,13 @@ public class MyIntArrayList {
   }
 
 
-
-
-
   public int size() {
     return this.size;
   }
 
 
-
   public int indexOf(int e) {
-    for (int i=0; i<this.size; i++) {
+    for (int i = 0; i < this.size; i++) {
       if (this.elements[i] == e) {
         return i;
       }
@@ -73,16 +67,14 @@ public class MyIntArrayList {
   }
 
 
-
   public boolean contains(int e) {
-    for (int i=0; i<this.size; i++) {
+    for (int i = 0; i < this.size; i++) {
       if (this.elements[i] == e) {
         return true;
       }
     }
     return false;
   }
-
 
 
   public int lastIndexOf(int e) {
@@ -97,70 +89,122 @@ public class MyIntArrayList {
   }
 
 
-
   public boolean remove(int e) {
     if (lastIndexOf(e) == this.size - 1) {
       size--;
       return true;
     }
-    if (this.contains(e)){
-      for (int i = indexOf(e); i < this.size; i++){
-        this.elements[i] = this.elements[i+1];
+    if (this.contains(e)) {
+      for (int i = indexOf(e); i < this.size; i++) {
+        this.elements[i] = this.elements[i + 1];
       }
       this.size--;
       return true;
     }
-      return false;
+    return false;
   }
-
 
 
   public int removeElementAtIndex(int index) {
     if (index < 0 || index >= this.size) {
       // should throw IndexOutOfBoundsException
       System.out.println("Throwing IndexOutOfBoundsException when trying to" +
-                         " remove element at position " + index);
+              " remove element at position " + index);
       return -1;
     }
     int removedElement = this.elements[index];
-    for (int i=index; i<this.size; i++) {
-      this.elements[i] = this.elements[i+1];
+    for (int i = index; i < this.size; i++) {
+      this.elements[i] = this.elements[i + 1];
     }
     this.size--;
     return removedElement;
   }
-  
+
+
   public void clear() {
     this.size = 0;
   }
 
-  public int get(int index){
+
+  public int get(int index) {
     return this.elements[index];
   }
-
 
 
   public int set(int index, int e) {
     if (index < 0 || index >= this.size) {
       // should throw IndexOutOfBoundsException
       System.out.println("Throwing IndexOutOfBoundsException when trying to set" +
-                         " the element " + e + " at position " + index);
+              " the element " + e + " at position " + index);
       return -1;
     }
-    
+
     int replacedElement = this.elements[index];
     this.elements[index] = e;
     return replacedElement;
   }
 
-  public boolean addAll(MyIntArrayList c){
-    if( c.size() == 0){
+
+  public boolean addAll(MyIntArrayList c) {
+    return addAll(this.size-1, c);
+    }
+
+
+  public boolean addAll(int index, MyIntArrayList c){
+    if (c.size() == 0) {
       System.out.println("The specified collection is empty.");
-      return false;}
+      return false;
+    }
 
+      if (index >= 0 && index <= this.size){
+        this.elements = Arrays.copyOf(this.elements,this.size + c.size() + 1);
+        this.size += c.size();
 
-
-
+        for ( int i = this.size - 1; i >= index; i--){
+          this.elements[i] = this.elements[i - c.size()];
+        }
+        for (int i = 0; i <= c.size; i++){
+          this.elements[index] = c.get(i);
+          index++;
+        }
+        return true;
+      }else{
+        System.out.println("Throwing IndexOutOfBoundsException when trying to add" +
+                " the collection " + c + " from position " + index);
+        return false;
+      }
   }
-  
+
+
+  public int[] toArray(){
+    int[] newArray = new int[this.size];
+
+    for (int i = 0; i < this.size-1; i++){
+      newArray[i] = this.elements[i];
+    }
+    return newArray;
+  }
+
+
+  public void ensureCapacity(int minCapacity){
+      this.elements = Arrays.copyOf(this.elements, minCapacity);
+  }
+
+
+  public void trimToSize(){
+      this.elements = Arrays.copyOf(this.elements, this.size + 1);
+    }
+
+  public void printArray(){
+
+    if (size != 0) {
+      System.out.println("The arrayList is: ");
+      for (int i = 0; i < size; i++){
+        System.out.print( this.elements[i] + " ");
+      }
+      System.out.println();
+    }else{
+      System.out.println("The arrayList is empty.\n");
+    }
+  }
 }
--- MODUL_2/src/Curs6/main.java
+++ MODUL_2/src/Curs6/main.java
@@ -5,7 +5,36 @@ import java.util.ArrayList;
 
 public class main {
     public static void main(String[] args) {
-        ArrayList<Double> doubleArrayList  = new ArrayList<Double>();
+        MyIntArrayList intArrayList1 = new MyIntArrayList(3);
+        MyIntArrayList intArrayList2 = new MyIntArrayList(3);
+
+
+        intArrayList1.add(22);
+        intArrayList1.add(33);
+        intArrayList1.add(44);
+
+        intArrayList1.printArray();
+
+        intArrayList1.add(223);
+        intArrayList1.add(224);
+        intArrayList1.add(225);
+
+        intArrayList1.add(2, 55);
+        intArrayList1.add(0, 11);
+        intArrayList1.add(7, 999);
+
+
+        intArrayList1.printArray();
+
+        System.out.println(intArrayList1.size());
+
+        intArrayList1.remove(225);
+        intArrayList1.remove(22);
+        intArrayList1.removeElementAtIndex(5);
+
+        intArrayList1.printArray();
+        System.out.println(intArrayList1.size());
+
 
     }
 }
